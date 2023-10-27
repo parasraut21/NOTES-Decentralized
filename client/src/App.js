0361140b3efffff56fcc4058dff08dd1786b3e4a
Paras Raut <parasraut2511@gmail.com>
2023-10-27 22:22:15 +0530

    updated code

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,8 @@
 import Notes from "./artifacts/contracts/Notes.sol/Notes.json";
 import { useState, useEffect } from "react";
 import { ethers } from "ethers";
-import Display from "./Display";
-import { NoteForm, NoteList } from "./NoteComponents";
+import Display from "./components/Display";
+import { NoteForm, NoteList } from "./components/NoteComponents";
 
 import "./App.css";
 
@@ -46,8 +46,34 @@ function App() {
   }, []);
   return (
     <>
+    
+<nav class="border-gray-200 bg-gray-50 dark:bg-gray-800 dark:border-gray-700">
+  <div class="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
+    <a href="#" class="flex items-center">
+        <img src="https://cryptologos.cc/logos/ethereum-eth-logo.png?v=026" class="h-8 mr-3" alt="Flowbite Logo" />
+        <span class="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">Ethereum Blockchain</span>
+    </a>
+    <button data-collapse-toggle="navbar-solid-bg" type="button" class="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600" aria-controls="navbar-solid-bg" aria-expanded="false">
+        <span class="sr-only">Open main menu</span>
+        <svg class="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 17 14">
+            <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 1h15M1 7h15M1 13h15"/>
+        </svg>
+    </button>
+    <div class="hidden w-full md:block md:w-auto" id="navbar-solid-bg">
+      <ul class="flex flex-col font-medium mt-4 rounded-lg bg-gray-50 md:flex-row md:space-x-8 md:mt-0 md:border-0 md:bg-transparent dark:bg-gray-800 md:dark:bg-transparent dark:border-gray-700">
+        <li>
+          <h4 href="#" class="block py-2 pl-3 pr-4 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 md:dark:text-blue-500 dark:bg-blue-600 md:dark:bg-transparent" aria-current="page"> 
+<b>Sepolia Testnet</b>
+          </h4>
+        </li>
+      
+      </ul>
+    </div>
+  </div>
+</nav>
+
       <div className="App">
-        <h1 style={{ color: "white" }}>Notes</h1>
+        <h1 style={{ color: "white" }}>Decentralized Notes</h1>
         <div class="bg"></div>
         <div class="bg bg2"></div>
         <div class="bg bg3"></div>
@@ -59,6 +85,28 @@ function App() {
         <NoteForm account={account} contract={contract} />
       <NoteList account={account} contract={contract} />
       </div>
+      
+<footer class="bg-white rounded-lg fixed bottom-0  shadow m-4 dark:bg-gray-800">
+    <div class="w-full mx-auto max-w-screen-xl p-4 md:flex md:items-center md:justify-between">
+      <span class="text-sm text-gray-500 sm:text-center dark:text-gray-400">© 2023 <a href="https://flowbite.com/" class="hover:underline">Notes™</a>. All Rights Reserved.
+    </span>
+    <ul class="flex flex-wrap items-center mt-3 text-sm font-medium text-gray-500 dark:text-gray-400 sm:mt-0">
+        <li>
+            <a href="#" class="mr-4 hover:underline md:mr-6 ">About</a>
+        </li>
+        <li>
+            <a href="#" class="mr-4 hover:underline md:mr-6">Privacy Policy</a>
+        </li>
+        <li>
+            <a href="#" class="mr-4 hover:underline md:mr-6">Licensing</a>
+        </li>
+        <li>
+            <a href="#" class="hover:underline">Contact</a>
+        </li>
+    </ul>
+    </div>
+</footer>
+
     </>
   );
 }
---
+++ client/src/components/NoteComponents.js
@@ -0,0 +1,157 @@
+import React, { useState } from "react";
+import { ethers } from "ethers";
+
+function NoteForm({ account, provider, contract }) {
+  const [note, setNote] = useState("");
+
+  const handleNoteChange = (e) => {
+    setNote(e.target.value);
+  };
+
+  const handleStoreNote = async () => {
+    if (!contract) {
+      console.error("Contract is not available");
+      return;
+    }
+
+    try {
+      await contract.storeNote(note);
+      setNote(""); // Clear the input field after storing the note
+    } catch (error) {
+      console.error("Error storing note:", error);
+    }
+  };
+
+  return (
+    <>
+   
+    <div className="bg-blue-200 p-4 rounded-lg  max-w-md mx-auto">
+      <input
+        className="w-full p-2 rounded border border-blue-400"
+        type="text"
+        placeholder="Enter your note"
+        value={note}
+        onChange={handleNoteChange}
+      />
+      <button
+        className="mt-2 bg-blue-500 hover:bg-blue-600 text-white py-2 px-4 rounded"
+        onClick={handleStoreNote}
+      >
+        Store Note
+      </button>
+    </div>
+    <br/>
+    </>
+  );
+}
+
+function NoteList({ account, contract }) {
+  const [notes, setNotes] = useState([]);
+
+  const handleRetrieveNotes = async () => {
+    if (!contract) {
+      console.error("Contract is not available");
+      return;
+    }
+
+    try {
+      const retrievedNotes = await contract.retrieveNotes();
+      setNotes(retrievedNotes.filter((note) => note !== ""));
+    } catch (error) {
+      console.error("Error retrieving notes:", error);
+    }
+  }
+
+  return (
+    <div className="bg-blue-200 p-4 rounded-lg max-w-md mx-auto">
+    <button
+      className="bg-blue-500 hover:bg-blue-600 text-white py-2 px-4 rounded"
+      onClick={handleRetrieveNotes}
+    >
+      Retrieve Notes
+    </button>
+    <div className="mt-4">
+      {notes.map((note, index) => (
+        <div
+          key={index}
+          className="bg-white rounded-lg p-4 my-2 border border-blue-400"
+        >
+          {note}
+        </div>
+      ))}
+    </div>
+  </div>
+  );
+}
+
+export { NoteForm, NoteList };
+
+// import React, { useState } from "react";
+// import { ethers } from "ethers";
+
+// function NoteForm({ account, provider, contract }) {
+//   const [note, setNote] = useState("");
+
+//   const handleNoteChange = (e) => {
+//     setNote(e.target.value);
+//   };
+
+//   const handleStoreNote = async () => {
+//     if (!contract) {
+//       console.error("Contract is not available");
+//       return;
+//     }
+
+//     try {
+//       await contract.storeNote(note);
+//       setNote(""); // Clear the input field after storing the note
+//     } catch (error) {
+//       console.error("Error storing note:", error);
+//     }
+//   };
+
+//   return (
+//     <div>
+   
+//       <input
+//         type="text"
+//         placeholder="Enter your note"
+//         value={note}
+//         onChange={handleNoteChange}
+//       />
+//       <button onClick={handleStoreNote}>Store Note</button>
+//     </div>
+//   );
+// }
+
+// function NoteList({ account, contract }) {
+//   const [notes, setNotes] = useState([]);
+
+//   const handleRetrieveNotes = async () => {
+//     if (!contract) {
+//       console.error("Contract is not available");
+//       return;
+//     }
+
+//     try {
+//       const retrievedNotes = await contract.retrieveNotes();
+//       setNotes(retrievedNotes.filter((note) => note !== ""));
+//     } catch (error) {
+//       console.error("Error retrieving notes:", error);
+//     }
+//   }
+
+//   return (
+//     <div>
+     
+//       <button onClick={handleRetrieveNotes}>Retrieve Notes</button>
+//       <ul>
+//         {notes.map((note, index) => (
+//           <li key={index}>{note}</li>
+//         ))}
+//       </ul>
+//     </div>
+//   );
+// }
+
+// export { NoteForm, NoteList };
